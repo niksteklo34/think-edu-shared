ed34d6e8c498930f265dc6c60413d34a69f592c2
Nikita <nikita@MacBook-Pro-Nikita.local>
2021-01-18 21:06:01 +0200

    Add Nikita Miroshnichenko to list

--- index.php
+++ index.php
@@ -7,5 +7,5 @@ echo "Nikita Test"
 echo "Malyshenko Alexandra, student";
 echo "Dashkovska Anastasia";
 echo "Коновалов Михаил Михайлович" . PHP_EOL;
-echo "Никита Мирошниченко Михайлович";
+echo "Никита Мирошниченко Михайлович" . "!";
 >>>>>>> 815f0706b9de5a92c07e327a047a52e372100828
