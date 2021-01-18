fa7ad640709b39332a903ebe48fb23bb9716f01e
Anastasia Dashkovska <anastasiia1529@gmail.com>
2021-01-18 18:50:35 +0200

    fix double lines

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 echo "Онищенко Богдан, Лектор";
 echo "Malyshenko Alexandra, student";
-echo "Онищенко Богдан, Лектор" . PHP_EOL;
 echo "Dashkovska Anastasia";
-echo "Онищенко Богдан, Лектор" . PHP_EOL;
 echo "Коновалов Михаил Михайлович" . PHP_EOL;
 echo "NIKITE";
